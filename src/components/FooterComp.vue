03ab0f3fbd9901fc70f347c77f7315661d17edf3
Andrea-Lulli <andrea.lulli89@gmail.com>
2022-11-24 10:46:36 +0100

    cambio img sfondo del footer

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,193 +1,175 @@
 <template>
-<footer>
+  <footer>
     <div class="conteiner">
-        <div class="DcComics">
+      <div class="DcComics">
         <div>
-            <ul>
-                <li class="titolo">DC COMICS</li>
-                <li>Characters</li>
-                <li>Comics</li>
-                <li>Movies</li>
-                <li>Tv</li>
-                <li>Games</li>
-                <li>Videos</li>
-                <li>News</li>
-            </ul>
-            <ul>
-                <li class="titolo">SHOP</li>
-                <li>Shop DC</li>
-                <li>Shop DC Collectibles</li>
-            </ul>
+          <ul>
+            <li class="titolo">DC COMICS</li>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>Tv</li>
+            <li>Games</li>
+            <li>Videos</li>
+            <li>News</li>
+          </ul>
+          <ul>
+            <li class="titolo">SHOP</li>
+            <li>Shop DC</li>
+            <li>Shop DC Collectibles</li>
+          </ul>
         </div>
         <div>
-            <ul>
-                <li class="titolo">DC</li>
-                <li>Tems of use</li>
-                <li>Privacy</li>
-                <li>Ad Choise</li>
-                <li>Adversting</li>
-                <li>Jobs</li>
-                <li>Subscriptions</li>
-                <li>Talent</li>
-                <li>CPSC Certificates</li>
-                <li>Ratings</li>
-                <li>Shop Help</li>
-                <li>Contact Us</li>
-            </ul>  
+          <ul>
+            <li class="titolo">DC</li>
+            <li>Tems of use</li>
+            <li>Privacy</li>
+            <li>Ad Choise</li>
+            <li>Adversting</li>
+            <li>Jobs</li>
+            <li>Subscriptions</li>
+            <li>Talent</li>
+            <li>CPSC Certificates</li>
+            <li>Ratings</li>
+            <li>Shop Help</li>
+            <li>Contact Us</li>
+          </ul>
         </div>
         <div>
-            <ul>
-                <li class="titolo">SITES</li>
-                <li>DC</li>
-                <li>MAD</li>
-                <li>DC Kids</li>
-                <li>DC Universe</li>
-                <li>DC Power Visa</li>               
-            </ul>  
+          <ul>
+            <li class="titolo">SITES</li>
+            <li>DC</li>
+            <li>MAD</li>
+            <li>DC Kids</li>
+            <li>DC Universe</li>
+            <li>DC Power Visa</li>
+          </ul>
         </div>
+      </div>
 
-    </div>
-
-    <div class="img-logo">
-        <img src="../assets/img/dc-logo-bg.png" alt="">
-    </div>
-
-
+      <div class="img-logo">
+        
+      </div>
     </div>
     <div class="footer-icon">
-        <div class="conteiner flex">
-            <div class="sign-up">
-                <a>SIGN-UP NOW!</a>
-            </div>
-            <div class="icon">
-                <span>FOLLOW US</span>
-                <img src="../assets/img/footer-facebook.png" alt="">
-                <img src="../assets/img/footer-periscope.png" alt="">
-                <img src="../assets/img/footer-twitter.png" alt="">
-                <img src="../assets/img/footer-youtube.png" alt="">
-                <img src="../assets/img/footer-pinterest.png" alt="">
-            </div>
+      <div class="conteiner flex">
+        <div class="sign-up">
+          <a>SIGN-UP NOW!</a>
         </div>
-
+        <div class="icon">
+          <span>FOLLOW US</span>
+          <img src="../assets/img/footer-facebook.png" alt="" />
+          <img src="../assets/img/footer-periscope.png" alt="" />
+          <img src="../assets/img/footer-twitter.png" alt="" />
+          <img src="../assets/img/footer-youtube.png" alt="" />
+          <img src="../assets/img/footer-pinterest.png" alt="" />
+        </div>
+      </div>
     </div>
-    
-</footer>
-
-
-    
+  </footer>
 </template>
 
 <script>
-
-
-    export default {
-        name: "FooterComp",
-        components: {
-    
-  }
-    }
+export default {
+  name: "FooterComp",
+  components: {},
+};
 </script>
 
 
 
 <style lang="scss" scoped>
-footer{
-    background-image:url(../assets/img/footer-bg.jpg) ;
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-}
-.conteiner{
-    width: 80%;
-    display: flex;
-    color: grey;
 
-    
+footer {
+  background-image: url(../assets/img/footer-bg.jpg);
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
 }
-
-.DcComics{
-    display: flex;
-    flex-basis: 40%;
-    margin: 25px 0;
+.conteiner {
+  width: 80%;
+  display: flex;
+  color: grey;
 }
 
-.img-logo{
-   flex-basis: 60%; 
-   justify-content: center;
-   display: flex;
-   
-   img{
+.DcComics {
+  display: flex;
+  flex-basis: 40%;
+  margin: 35px 0;
+}
+
+.img-logo {
+  flex-basis: 60%;
+  display: flex;
+  background-image: url(../assets/img/dc-logo-bg.png);
+  background-repeat: no-repeat;
+  background-position: center;
+
+  img {
     width: 50%;
-   }
+  }
 }
 
-ul{
-    list-style-type: none;
-    li{
-        margin: 5px 0px;
-        margin-right: 25px;
+ul {
+  list-style-type: none;
+  li {
+    margin: 8px 0px;
+    margin-right: 25px;
 
-        font-size: 12px;
-    }
+    font-size: 12px;
+  }
 }
-.titolo{
-   color: white;
-   font-weight: 700;
-   font-size: 16px;
-   margin: 10px 0;
+.titolo {
+  color: white;
+  font-weight: 700;
+  font-size: 16px;
+  margin: 10px 0;
 }
 
-.footer-icon{
-    background-color: rgba(48,48,48,255)  ;
-    width: 100%;
-    padding: 20px 0;
+.footer-icon {
+  background-color: rgba(48, 48, 48, 255);
+  width: 100%;
+  padding: 20px 0;
+  display: flex;
+  justify-content: center;
+
+  .flex {
     display: flex;
-    justify-content: center;
-    
-    
-
-.flex{
-display: flex;
-width: 80%;
-align-items: center;
-    
-
-}
-}
-
-.sign-up{
-    flex-basis: 20%;
-    display: flex;
-    justify-content: start;
-    
-}
-
-.icon{
-    flex-basis: 79%;
-    display: flex;
-    justify-content: end;
+    width: 80%;
     align-items: center;
-    
+  }
 }
 
-div{
-    span{
-        color: rgba(2,130,249,255);
-        margin-right: 10px;
-        font-weight: 800;
-        font-size: 20px;
-    }
-    img {
-margin: 0 8px;
-    };
+.sign-up {
+  flex-basis: 20%;
+  display: flex;
+  justify-content: start;
 }
 
-div{
-    a{
-        border: 2px solid rgba(2,130,249,255);
-        padding: 10px 10px;
-        color: white;
-    }
+.icon {
+  flex-basis: 79%;
+  display: flex;
+  justify-content: end;
+  align-items: center;
 }
 
+div {
+  span {
+    color: rgba(2, 130, 249, 255);
+    margin-right: 10px;
+    font-weight: 800;
+    font-size: 20px;
+  }
+  img {
+    margin: 0 8px;
+  }
+}
+
+div {
+  a {
+    border: 2px solid rgba(2, 130, 249, 255);
+    padding: 10px 10px;
+    color: white;
+  }
+}
 </style>
