49ab9fd2e8499fcac6c6338a11e7c196c031eb8f
Andrea-Lulli <andrea.lulli89@gmail.com>
2022-11-24 18:13:35 +0100

    inserite card

---
+++ src/components/MainCardComp.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card-fumetti">
+    <img :src="Prodotti.thumb" />
+    <p>{{ Prodotti.series }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainCardComp",
+  props: {
+    Prodotti: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card-fumetti {
+  flex-basis: 15%;
+  margin: 20px 0;
+  color: white;
+  text-align: center;
+}
+
+div {
+  img {
+    width: 100%;
+  }
+}
+</style>
